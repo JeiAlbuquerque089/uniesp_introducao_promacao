366a8e16deb34f5cdef62866989bfcd2176588f7
Jeimison Albuquerque <112208779+JeiAlbuquerque089@users.noreply.github.com>
2022-10-09 20:20:10 -0300

    Add files via upload

---
+++ # Ted-09.py
@@ -0,0 +1,27 @@
+# Ted-09
+#Use um dicionário para armazenar informações sobre uma pessoa que você conheça. 
+# Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive. 
+# Você deverá ter chaves como primeiro_nome, sobrenome, idade, e cidade. 
+# por fim, mostre cada informação armazenada em seu dicionário.
+
+
+Jeimison = {"Primeiro nome": "Jeimison", "Sobrenome": "Albuquerque", "idade": 33, "cidade": "Cabedelo-PB"}
+print(Jeimison["Primeiro nome"])
+print(Jeimison["Sobrenome"])
+print(Jeimison["idade"])
+print(Jeimison["cidade"])
+
+
+#Questão 2 - Use um dicionário para armazenar os números favoritos de algumas pessoas. Esoclha cinco colegas,
+#e pergunte quais seus número favoritos. 
+#Use seus nomes para serem as chaves de cada número favorito.
+#Ao final, exiba o nome de cada pessoas e seu número favorito.
+
+
+Número = {"Jei": 7, "Finho": 50, "Cris": 38, "Brian": 24, "Pedrinho": 15}
+
+print(Número["Jei"])
+print(Número["Finho"])
+print(Número["Cris"])
+print(Número["Brian"])
+print(Número["Pedrinho"])
